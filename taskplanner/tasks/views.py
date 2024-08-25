e3a96daa8ab27a6493dad7be52eae0c111415b54
Madushan-Jaya-Sri <111506676+Madushan-Jaya-Sri@users.noreply.github.com>
2024-08-25 15:12:24 +0530

    initial commit

---
+++ taskplanner/tasks/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .models import Task
+from .forms import TaskForm
+import datetime
+
+@login_required
+def dashboard(request):
+    tasks = Task.objects.filter(user=request.user)
+    current_time = datetime.datetime.now().time()
+    
+    # Change background based on the time of the task
+    if current_time < datetime.time(11, 0):
+        background_image = 'backgroundimg1.jpg'
+    else:
+        background_image = 'backgroundimg2.jpg'
+
+    return render(request, 'tasks/dashboard.html', {
+        'tasks': tasks,
+        'background_image': background_image,
+    })
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('dashboard')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/add_task.html', {'form': form})
+
+
+
+@login_required
+def edit_task(request, task_id):
+    task = Task.objects.get(id=task_id, user=request.user)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+@login_required
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id, user=request.user)
+    task.delete()
+    return redirect('dashboard')
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('dashboard')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'tasks/login.html', {'form': form})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'tasks/register.html', {'form': form})
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('login')
